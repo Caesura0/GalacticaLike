9ef435c54a1817142728b484888fadf539163d61
LENOVO-JAKE-S\jskar <jskarupa1@gmail.com>
2024-05-18 13:55:13 -0400

    commit to switch computers

--- Assets/Scripts/BossSpawner.cs
+++ Assets/Scripts/BossSpawner.cs
@@ -8,10 +8,11 @@ public class BossSpawner : MonoBehaviour
     bool bossDestroyed = false;
     [SerializeField] Enemy boss;
 
+    [SerializeField] List<GameObject> pathList;
 
     public void SpawnBoss()
     {
-
+        Instantiate(boss, transform.position, Quaternion.Euler(0, 0, 180)) ;
     }
 
 
